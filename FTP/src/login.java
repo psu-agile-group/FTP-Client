c11eb6222cc1e149c31d7898d35edb45ba0e1528
ronniesong0809 <ronniesong0809@gmail.com>
2019-07-04 13:11:32 -0700

    feat: #35 a simple Shell interface, and #28 add a feature that allows the system to list directories and files on a remote server

--- FTP/src/login.java
+++ FTP/src/login.java
@@ -1,52 +1,101 @@
-
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 
 public class login {
 
-
-        private static void show_Message_fromServer(FTPClient ftpClient) {
-            String[] replies = ftpClient.getReplyStrings();
-            if (replies != null && replies.length > 0) {
-                for (int i = 0; i < replies.length; ++i) {
-                    System.out.println("SERVER: " + replies[i]);
-                }
-            }
-        }
-        public static void main(String[] args) {
-            // Variable from the command line argument
-
-            String server = args[0];//"193.219.28.2";
-            int port = Integer.parseInt(args[1]); //21;
-            String user = args[2];//"anonymous";
-            String pass = args[3]; //"me@nowhere.com";
-            FTPClient ftpClient = new FTPClient();
-
-            try {
-                ftpClient.connect(server, port);
-                show_Message_fromServer(ftpClient);
-                int replyCode = ftpClient.getReplyCode();
-
-                // FTPReply stores a set of constants for FTP reply codes.
-                //Determine if a reply code is a positive completion response.
-                // All codes beginning with a 2 are positive completion responses
-                //True if a reply code is a positive completion response, false if not.
-                if (!FTPReply.isPositiveCompletion(replyCode)) {
-                    System.out.println("Operation failed. Server reply code: " + replyCode);
-                    return;
-                }
-                boolean success = ftpClient.login(user, pass); // login method in the library
-                show_Message_fromServer(ftpClient); // show messg from the server after log in.
-                if (!success) {
-                    System.out.println("Could not login to the server");
-                } else {
-                    System.out.println("LOGGED IN SERVER");
-                }
-            } catch (IOException e) {
-                System.out.println("Oops! Something wrong happened");
-                e.printStackTrace();
+    private static void show_Message_fromServer(FTPClient ftpClient) {
+        String[] replies = ftpClient.getReplyStrings();
+        if (replies != null && replies.length > 0) {
+            for (int i = 0; i < replies.length; ++i) {
+                System.out.println("SERVER: " + replies[i]);
             }
         }
     }
+    private static void shell(FTPClient ftpClient) throws IOException {
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        String command;
 
+        while (true) {
+            System.out.print("\nFTP Shell >> ");
+            command = console.readLine();
+            String[] lineSplit = command.split(" ");
+
+            if (command.equals("")) {
+                continue;
+
+            }else if(lineSplit[0].equals("ls")) {
+                list_directories_fromServer(ftpClient);
+
+            }else if(lineSplit[0].equals("cd")) {
+                //TODO
+                /*String newCommand = "";
+                for (String line : lineSplit) {
+                    newCommand += line;
+                }*/
+                cd_directories_fromServer(ftpClient, lineSplit[1]);
+
+            }else if(command.equalsIgnoreCase("exit")||command.equalsIgnoreCase("quit"))  {
+                System.out.println("Goodbye");
+                System.exit(0);
+             }
+        }
+    }
+
+    private static void list_directories_fromServer(FTPClient ftpClient){
+        ftpClient.enterLocalPassiveMode();
+        try{
+            FTPFile[] listDirectories = ftpClient.listDirectories();
+            if (listDirectories != null) {
+                for (FTPFile file : listDirectories) {
+                    System.out.print(file.getName()+'\t');
+                }
+            }
+        }catch (IOException e) {
+            System.out.println("Oops! Something wrong happened: " + e);
+        }
+    }
+
+    private static void cd_directories_fromServer(FTPClient ftpClient, String lineSplit){
+        //TODO
+    }
+
+    public static void main(String[] args) {
+        // Variable from the command line argument
+        // ftpupload.net 21 epiz_24139835 OkmvEHWbl4HFJ8a
+        String server = args[0];//"193.219.28.2";
+        int port = Integer.parseInt(args[1]); //21;
+        String user = args[2];//"anonymous";
+        String pass = args[3]; //"me@nowhere.com";
+        FTPClient ftpClient = new FTPClient();
+
+        try {
+            ftpClient.connect(server, port);
+            show_Message_fromServer(ftpClient);
+            int replyCode = ftpClient.getReplyCode();
+
+            // FTPReply stores a set of constants for FTP reply codes.
+            if (!FTPReply.isPositiveCompletion(replyCode)) {
+                System.out.println("Operation failed. Server reply code: " + replyCode);
+                return;
+            }
+            boolean success = ftpClient.login(user, pass); // login method in the library
+            show_Message_fromServer(ftpClient); // show messg from the server after log in.
+            if (!success) {
+                System.out.println("Could not login to the server");
+            } else {
+                System.out.println("LOGGED IN SERVER");
+            }
+
+            shell(ftpClient);
+
+        } catch (IOException e) {
+            System.out.println("Oops! Something wrong happened");
+            e.printStackTrace();
+        }
+    }
+}
