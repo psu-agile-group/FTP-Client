98c63cc90910e6ff0bd60f9225027f2c03383def
David Post <david@ohyonghao.com>
2019-07-14 14:25:18 -0700

    Removed headers no longer needed in base class
    Now that we have split out to other classes we don't need it in here.

--- src/main/java/com/psuagilegroup/Command.java
+++ src/main/java/com/psuagilegroup/Command.java
@@ -2,10 +2,6 @@ package com.psuagilegroup;
 
 import org.apache.commons.net.ftp.FTPClient;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
 public abstract class Command {
     FTPClient ftpClient;
     public Command(FTPClient ftpClient)
