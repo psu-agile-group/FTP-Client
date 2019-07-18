b2163b06ea2c92b52573a7dc60774aba8d1d408b
Vikram Guhilot <guhilot@pdx.edu>
2019-07-18 15:48:47 -0700

    remdir

---
+++ src/main/java/com/psuagilegroup/rmdirCommand.java
@@ -0,0 +1,54 @@
+package com.psuagilegroup;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.IOException;
+
+public class rmdirCommand extends Command {
+
+    public rmdirCommand(FTPClient ftpClient) {
+        super(ftpClient);
+    }
+
+    @Override
+    public String help() {
+        return "rmdir\tRemove directory.";
+    }
+
+    @Override
+    public FTPSession run(FTPSession currentSession, String[] args) {
+        remove_dir_on_server(args[1]);
+        return currentSession;
+    }
+
+    private void remove_dir_on_server(String dirToRemove) {
+        ftpClient.enterLocalActiveMode();
+        /*boolean flag = false;
+        try {
+            flag = ftpClient.changeWorkingDirectory("/htdocs");
+            show_Message_fromServer(ftpClient);
+            if(flag)
+                System.out.println("hit");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        //System.out.printf("The directory is %s", dirToRemove);
+        //System.out.printf("%s", dirToRemove);
+        try {
+            boolean deleted = ftpClient.removeDirectory(dirToRemove);
+            if (deleted) {
+                System.out.println("The directory was removed successfully.");
+            } else {
+                System.out.println("Could not delete the directory, it may not be empty.");
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Oh no, there was an error: " + ex.getMessage());
+            ex.printStackTrace();
+
+        }
+
+
+    }
+}
