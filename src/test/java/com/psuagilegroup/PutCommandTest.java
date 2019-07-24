0ad876e89454ae7211638a8fc5cb3146edf09fc6
ddveloper <44758313+ddveloper@users.noreply.github.com>
2019-07-23 17:36:57 -0700

    update test to Junit 4.12

--- src/test/java/com/psuagilegroup/PutCommandTest.java
+++ src/test/java/com/psuagilegroup/PutCommandTest.java
@@ -1,35 +1,39 @@
 package com.psuagilegroup;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
 
 import org.apache.commons.net.ftp.FTPClient;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.HashMap;
-import java.io.ByteArrayInputStream;
 
 /**
  * Unit test for simple PutCommand.
  */
+@RunWith(JUnit4.class)
 public class PutCommandTest
-        extends TestCase
 {
-    FTPClient ftpClient;
-    FTPSession currentSession;
-    HashMap<String, Command> commands;
+    private FTPClient ftpClient;
+    private FTPSession currentSession;
+    private HashMap<String, Command> commands;
 
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public PutCommandTest( String testName )
-    {
-        super( testName );
+    private String finaOutString;
+    private String testString[];
+    private InputStream originalIn;
+    private PrintStream originalOut;
+    private ByteArrayInputStream inContent;
+    private ByteArrayOutputStream outContent;
 
+    @Before
+    public void init() {
         // init ftpClient
         ftpClient = new FTPClient();
 
@@ -40,77 +44,71 @@ public class PutCommandTest
         // init useful commands
         commands = new HashMap<>();
         commands.put("login", new loginCommand(ftpClient));
-        commands.put("logout", new logoutCommand(ftpClient));
         commands.put("cd", new cdCommand(ftpClient));
-        commands.put("ls", new lsCommand(ftpClient));
-        commands.put("rls", new rlsCommand(ftpClient));
         // my command to test
         commands.put("put", new putCommand(ftpClient));
 
         // login before test
         commands.get("login").run(currentSession, new String[0]);
-        String [] testStr = {"cd", "htdocs"};
-        commands.get("cd").run(currentSession, testStr);
+        testString = new String[]{"cd", "htdocs"};
+        commands.get("cd").run(currentSession, testString);
+
+        // make sure cd to htdocs works
         assertEquals("", currentSession.output);
 
+        originalIn = System.in;
+        originalOut = System.out;
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        finaOutString = "";
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( PutCommandTest.class );
+    @After
+    public void cleanup() {
+        // optionally, reset System.in to its original
+        System.setIn(originalIn);
+        System.setOut(originalOut);
+        System.out.println(finaOutString);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testPutCommand()
     {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-        PrintStream originalOut = System.out;
-        PrintStream originalErr = System.err;
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
-        String finalOutput = "";
-
-
         // local file does not exist
-        String [] testStr1 = {"put", "UnitTest/file1.html", "UnitTest"};
-        commands.get("put").run(currentSession, testStr1);
+        testString = new String[]{"put", "UnitTest/file1.html", "UnitTest"};
+        commands.get("put").run(currentSession, testString);
         assertTrue(outContent.toString().contains(" - local: \"UnitTest/file1.html\" is invalid."));
-        finalOutput += outContent; outContent.reset();
+        finaOutString += outContent; outContent.reset();
 
         // remote folder does not exist
-        String [] testStr2 = {"put", "UnitTest/file.html", "UnitTest1"};
-        commands.get("put").run(currentSession, testStr2);
+        testString = new String[]{"put", "UnitTest/file.html", "UnitTest1"};
+        commands.get("put").run(currentSession, testString);
         assertTrue(outContent.toString().contains(" - remote folder: \"UnitTest1\" is invalid."));
-        finalOutput += outContent; outContent.reset();
+        finaOutString += outContent; outContent.reset();
 
         // put single file pass
-        ByteArrayInputStream in1 = new ByteArrayInputStream("y".getBytes());
-        System.setIn(in1);
-        String [] testStr3 = {"put", "UnitTest/file.html", "UnitTest"};
-        commands.get("put").run(currentSession, testStr3);
+        inContent = new ByteArrayInputStream("y".getBytes());
+        System.setIn(inContent);
+        testString = new String[]{"put", "UnitTest/file.html", "UnitTest"};
+        commands.get("put").run(currentSession, testString);
         assertTrue(outContent.toString().contains(" - from : [UnitTest/file.html] to [UnitTest/file.html], upload ok."));
-        finalOutput += outContent; outContent.reset();
+        finaOutString += outContent; outContent.reset();
 
         // put multiple files pass
-        ByteArrayInputStream in2 = new ByteArrayInputStream("yes".getBytes());
-        System.setIn(in2);
-        String [] testStr4 = {"put", "UnitTest/file.html", "UnitTest/file2.html", "UnitTest"};
-        commands.get("put").run(currentSession, testStr4);
+        inContent = new ByteArrayInputStream("yes".getBytes());
+        System.setIn(inContent);
+        testString = new String[]{"put", "UnitTest/file.html", "UnitTest/file2.html", "UnitTest"};
+        commands.get("put").run(currentSession, testString);
         assertTrue(outContent.toString().contains(" - from : [UnitTest/file.html] to [UnitTest/file.html], upload ok."));
         assertTrue(outContent.toString().contains(" - from : [UnitTest/file2.html] to [UnitTest/file2.html], upload ok."));
-        finalOutput += outContent; outContent.reset();
+        finaOutString += outContent; outContent.reset();
 
         // put folder pass
-        ByteArrayInputStream in3 = new ByteArrayInputStream("yes".getBytes());
-        System.setIn(in3);
-        String [] testStr5 = {"put", "UnitTest/folder", "UnitTest"};
-        commands.get("put").run(currentSession, testStr5);
+        inContent = new ByteArrayInputStream("yes".getBytes());
+        System.setIn(inContent);
+        testString = new String[]{"put", "UnitTest/folder", "UnitTest"};
+        commands.get("put").run(currentSession, testString);
         assertTrue(outContent.toString().contains(" - from : [UnitTest/folder/hi test/README_2.md] to [UnitTest/folder/hi test/README_2.md], upload ok."));
         assertTrue(outContent.toString().contains(" - from : [UnitTest/folder/README.md] to [UnitTest/folder/README.md], upload ok."));
         assertTrue(outContent.toString().contains(" - from : [UnitTest/folder/README00 1.md] to [UnitTest/folder/README00 1.md], upload ok."));
@@ -118,12 +116,7 @@ public class PutCommandTest
         assertTrue(outContent.toString().contains(" - from : [UnitTest/folder/test22/README_4 - Copy.md] to [UnitTest/folder/test22/README_4 - Copy.md], upload ok."));
         assertTrue(outContent.toString().contains(" - from : [UnitTest/folder/test22/test 44/README_6 - Copy.md] to [UnitTest/folder/test22/test 44/README_6 - Copy.md], upload ok."));
         assertTrue(outContent.toString().contains(" - from : [UnitTest/folder/test22/test33/README_5.md] to [UnitTest/folder/test22/test33/README_5.md], upload ok."));
-        finalOutput += outContent; outContent.reset();
+        finaOutString += outContent; outContent.reset();
 
-        // optionally, reset System.in to its original
-        System.setIn(System.in);
-        System.setOut(originalOut);
-        System.setErr(originalErr);
-        System.out.println(finalOutput);
     }
 }
